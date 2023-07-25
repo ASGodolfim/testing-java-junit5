e86659ec5d3bf662020a5ed362b17bacf8d93aa6
ASGodolfim <stonegodolfim@gmail.com>
2023-07-25 14:04:19 -0300

    test: person groupedAssertions

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,7 +1,21 @@
 package guru.springframework.sfgpetclinic.model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
+
+    @Test
+    void groupedAssertions(){
+        //given
+        Person person = new Person(1l, "Joe", "Buck");
+
+        //then
+        assertAll("Test Props Set",
+                () -> assertEquals(person.getFirstName(), "Joe"),
+                () -> assertEquals(person.getLastName(), "Buck")
+        );
+    }
 
 }
