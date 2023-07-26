51d49bdead7d77ec9938dd56d326c25db0e04443
ASGodolfim <stonegodolfim@gmail.com>
2023-07-26 14:27:46 -0300

    build: ResourcesTest and OwnerTest CSV From File Test

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -4,6 +4,7 @@ import guru.springframework.sfgpetclinic.ModelTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -56,6 +57,13 @@ class OwnerTest implements ModelTest {
             "MI, 3, 1"
     })
     void csvInputTest(String stateName, int val, int val2) {
+        System.out.println(stateName + " = " + val + ":" + val2);
+    }
+
+    @DisplayName("CSV From File Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @CsvFileSource(resources = "/input.csv", numLinesToSkip = 1)
+    void csvFromFileTest(String stateName, int val, int val2) {
         System.out.println(stateName + " = " + val + ":" + val2);
     }
 }
