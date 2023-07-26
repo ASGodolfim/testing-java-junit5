57e71d683a9ca522c8dcf27192d17aae46ab6b5b
ASGodolfim <stonegodolfim@gmail.com>
2023-07-26 14:11:49 -0300

    test: OwnerTest ParametrizedTest

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -2,6 +2,8 @@ package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -29,4 +31,9 @@ class OwnerTest implements ModelTest {
         assertThat(owner.getCity(), is("Key West"));
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"Spring", "Framework", "Guru"})
+    void testValueSource(String val) {
+        System.out.println(val);
+    }
 }
